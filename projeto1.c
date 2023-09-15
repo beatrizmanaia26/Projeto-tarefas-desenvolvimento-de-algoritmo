ac329e54bf1c9e7fbe437035b241ec3ee3e31bff
Beatriz Manaia Lourenço Berto <130764598+beatrizmanaia26@users.noreply.github.com>
2023-09-15 18:46:24 -0300

    commit 2

--- projeto1.c
+++ projeto1.c
@@ -1,2 +1,12 @@
 //Beatriz Manaia Lourenço Berto      RA: 24.123.013-5
 //Luana Bortko Rodrigues             RA: 24.123.006-9
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    struct tarefas{
+        char lista[100];
+        
+    }
+}
