6dd6d82ae8770b6d436835000b36a503e4e31f23
Beatriz Manaia Lourenço Berto <130764598+beatrizmanaia26@users.noreply.github.com>
2023-10-01 18:58:32 -0300

    Update projeto1.h

--- projeto1.h
+++ projeto1.h
@@ -1,7 +1,7 @@
 
 typedef struct{
     int prioridade;
-    char categoria[100];
+    char categoria[100];//struct em categoria pra ter mais opcoes
     char descricao[300];
 }tarefa;
 
@@ -10,10 +10,10 @@ typedef struct{ //fwrite desse struct
     int quantidade; //fazer for (ultima tarefa que adicionei (comeca com 0 e vai add)
 }lista_tarefas;
 
-//*Lt-> alterar valor só na funcao
+//int pq retorna numero (1 ou 0)
 int cria_tarefa(lista_tarefas *Lt);
 int deleta_tarefa(lista_tarefas *Lt);
 int listar_tarefas(lista_tarefas Lt);
 void print_menu(); 
-int ler_arquivo(lista_tarefas *Lt, char nome[]); //fread
-int salva_tarefa(lista_tarefas Lt, char nome[]); //fwrite
+int ler_arquivo(lista_tarefas *Lt, char nome[]);
+int salva_tarefa(lista_tarefas Lt, char nome[]);
