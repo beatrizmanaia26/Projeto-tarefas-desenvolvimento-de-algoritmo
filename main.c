c931c80d4bb51ab02c5078570a8aef43c19f9661
Beatriz Manaia Lourenço Berto <130764598+beatrizmanaia26@users.noreply.github.com>
2023-10-01 18:58:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,17 +3,17 @@
 #include <string.h>
 
 int main(){
-    char arquivo[]= "tarefa";
+    char arquivo[]= "tarefa"; 
     int codigo; //armazena codigo de retorno das funcoes
     lista_tarefas Lt;
-    codigo = ler_arquivo(&Lt, arquivo);
+    codigo = ler_arquivo(&Lt, arquivo); //chama a funcao de ler arquivo
     if( codigo == 1){
         Lt.quantidade = 0; //limpa memoria 
     }
     int opcao;
     do{
         print_menu();
-        printf("Digite o numero da opcao desejada:");
+        printf("Digite o numero da opcao desejada: ");
         scanf("%d", &opcao);
     if (opcao == 0){
     }else if(opcao == 1){
@@ -21,13 +21,13 @@ int main(){
     }else if(opcao == 2){
         deleta_tarefa(&Lt);
     }else if(opcao == 3){
-        lista_tarefas(Lt);
+        listar_tarefas(Lt);
     }else{
         printf("Opcao escolhida nao existe\n");
     }
     }while(opcao != 0); //loop
 
-    codigo = salva_tarefa(Lt, arquivo);
+    codigo = salva_tarefa(Lt, arquivo);    //chama a funcao ->salva tarefa
     if(codigo != 0){
         printf("Erro ao salvar as tarefas");
     }
